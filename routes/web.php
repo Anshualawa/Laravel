15e0414f6fbdc70ca6a4f2eaa7808179fbaac80d
AnshuAlawa <alawapappu@gmail.com>
2023-06-16 14:10:03 +0530

    route change

--- routes/web.php
+++ routes/web.php
@@ -12,37 +12,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/home', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
-    return view('frontend');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/nav-bar', function () {
-    return view(('nav-bar'));
-});
-
-Route::get('/tv-shows', function () {
-    return view('tv-shows');
-});
-
-Route::any('/test', function () {
-    echo 'Testing the route';
-});
-// POST Method 
-
-Route::post('/test', function () {
-    echo 'Testing the route';
-});
-
-Route::get('/demo/{name}/{id?}', function ($name, $id = null) {
-    $data = compact('name', 'id');
-    return view('demo')->with($data);
+Route::get('/{name?}', function ($name) {
+    return view('home');
 });
